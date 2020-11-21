f25a4ce7ca0a5913717a1ab8f66868a97881c3bd
JohnDeVries <jdevries3133@gmail.com>
2020-11-20 19:02:36 -0500

    misc very small changes

--- zoom_attendance_reporter/services.py
+++ zoom_attendance_reporter/services.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from django.contrib.auth import get_user_model
-from teacherHelper.zoom_attendance_report import ExcelWriter, MeetingSet
+from teacherHelper.zoom_attendance_report import WorkbookWriter, MeetingSet
 
 from .models import MeetingCompletedReport
 
@@ -15,7 +15,8 @@ def make_meeting_set(*, data: List[bytes], user: get_user_model()) -> MeetingSet
     Note: form has validated that no file is greater than 1 mb, so file.read()
     is safe.
     """
-    meeting_set = MeetingSet([str(f.read(), 'utf-8-sig') for f in data])
+    meeting_set = MeetingSet(data := [str(f.read(), 'utf-8-sig') for f in data])
+
     # MeetingCompletedReport provides progress to the frontend.
     # Won't work on dev server because it can't handle concurrent request.
     for meeting in meeting_set.process():
@@ -23,7 +24,9 @@ def make_meeting_set(*, data: List[bytes], user: get_user_model()) -> MeetingSet
             topic=meeting.topic,
             meeting_time=meeting.datetime.date()
         )
+
+
     # delete all MeetingCompletedReport objects once processing is finished
+    # user will only be fetching them to get updates on processing progress.
     MeetingCompletedReport.objects.filter(owner=user).delete()
     return meeting_set
-
